2a465082659effe7de5e990abff3075f8e8410af
Robert Ohly <ohly0001@algonquinlive.com>
2025-12-10 21:33:11 -0500

    Updated

--- v2/mailbot_db.py
+++ v2/mailbot_db.py
@@ -2,8 +2,6 @@ from mysql.connector import connect, Error
 from mailbot_logging import Logger
 
 class DB:
-    SELECT_REG_USERS = "SELECT user_id, email_address, alias_name, registered_on FROM registered_users;"
-    
     def __init__(self, mysql_conn_params):
         self.logger = Logger(self)
         
@@ -33,23 +31,75 @@ class DB:
                 dictionary=True,
                 read_timeout=5, 
                 write_timeout=5)
-            
+             
         except Error as err:
             self.logger.error("Failed to connect/reconnect to MySQL", err)
             exit(2)
     
-    def select_registered_users(self):
+    def select_registered_users(self) -> list[]:
         self.connect_mysql()
-        
+         
         try:
-            self.mycursor.execute(self.SELECT_REG_USERS)
+            self.mycursor.execute("SELECT user_id, email_address, alias_name, registered_on FROM registered_users;")
             rows = self.mycursor.fetchall()
+            self.mydb.commit()
             self.logger.info(f"Selected {self.mycursor.rowcount} registered users")
             return rows
         
         except Error as err:
+            self.mydb.rollback()
             self.logger.error("Failed to select registered users", err)
             return []
+        
+    def update_thread_bot_reply_mode(self, thread_id: int, bot_reply_mode: str):
+        self.connect_mysql()
+        
+        try:
+            self.mycursor.execute("UPDATE email_thread SET bot_reply_mode = %s WHERE thread_id = %s;", 
+                                  (bot_reply_mode, thread_id))
+            self.mydb.commit()
+            self.logger.info(f"Updated {self.mycursor.rowcount} thread")
+            return self.mycursor.rowcount > 0
+        
+        except Error as err:
+            self.mydb.rollback()
+            self.logger.error("Failed to update thread", err)
+            return False
+        
+    def update_thread_bot_filter_mode(self, thread_id: int, bot_filter_mode: str):
+        self.connect_mysql()
+        
+        try:
+            self.mycursor.execute("UPDATE email_thread SET bot_filter_mode = %s WHERE thread_id = %s;", 
+                                  (bot_filter_mode, thread_id))
+            self.mydb.commit()
+            self.logger.info(f"Updated {self.mycursor.rowcount} thread")
+            return self.mycursor.rowcount > 0
+        
+        except Error as err:
+            self.mydb.rollback()
+            self.logger.error("Failed to update thread", err)
+            return False
+        
+    def delete_registered_user(self, user_id=None, email_address=None, alias=None) -> False:
+        self.connect_mysql()
+        
+        try:
+            if user_id is not None:
+                self.mycursor.execute("DELETE FROM registered_users WHERE user_id = %s;", (user_id,))
+            elif email_address is not None:
+                self.mycursor.execute("DELETE FROM registered_users WHERE email_addres = %s;", (email_address,))
+            elif alias is not None:
+                self.mycursor.execute("DELETE FROM registered_users WHERE alias = %s;", (alias,))
+            
+            self.mydb.commit()
+            self.logger.info(f"Deleted {self.mycursor.rowcount} registered users")
+            return self.mycursor.rowcount > 0
+        
+        except Error as err:
+            self.mydb.rollback()
+            self.logger.error("Failed to delete registered user", err)
+            return False
         
     def close(self):
         try:
@@ -61,6 +111,7 @@ class DB:
             
         try:
             if self.mydb and self.mydb.is_connected():
+                self.mydb.rollback()
                 self.mydb.close()
             self.logger.info("Disconnected from MySQL")
                 
--- v2/mailbot_v2.py
+++ v2/mailbot_v2.py
@@ -64,6 +64,58 @@ class MailBot:
         self.logger.info('Running')
         self.logger.flush()
         
+    def process_commands(self, email_address: str, body_text: str):
+        for line in body_text.strip().split('\n'):
+            if line.startswith('!'):
+                cmd = line.removeprefix('!').strip().lower()
+                if cmd == 'deregister': 
+                    # Remove self from whitelist (equivelant to delete account)
+                    self.db.delete_registered_user(email_address=email_address)
+                    
+                elif cmd in ['brief', 'stats']: 
+                    # Get thread count, message count, (and other client details)
+                    pass
+                elif cmd in ['help', '?']: 
+                    # Read user-level command list from help file 1 and send
+                    pass
+                
+            elif line.startswith('/'):
+                cmd = line.removeprefix('/').strip().lower()
+                
+                if cmd.startswith('mode') and cmd.endswith('natural', 'lazy', 'eager', 'idle'):
+                    # Natrual: Respond only when a certain heuristic threshold is met (mimicking interest and converstional relevance)
+                    # Lazy: Respond only when the bots name `Mistral` or a command is included
+                    # Eager: Responds to every user thread received
+                    # Idle: Don't respond but persist thread to db as received
+                    # Idle: Ignore commands into switched from off to other mode
+                    
+                    mode = cmd.split(maxsplit=1)[1]
+                    self.db.update_thread_bot_reply_mode(-1, mode)
+                
+                if cmd.startswith('filter'):
+                    cmd = cmd.removeprefix('filter').strip()
+                
+                    if cmd.startswith('include'): #`me` is alias for self, `cc` is for all cc people, comma seperated, `all`` removes filtering
+                        # Only responds to people in inclusion list, sets to thread whitelisting
+                        pass
+                    elif cmd.startswith('exclude'): #`me`` is alias for self, `cc` is for all cc people, comma seperated, `all`` removes filtering
+                        # Only ignore people in exclusion list, sets to thread blacklisting (can be used to 'shun' cc recipients)
+                        pass
+                    
+                # can also use: "Mistral, please provide a recap" etc, but this forces a particular response
+                elif cmd == 'recap': 
+                    # Request strict recap of conversation
+                    pass
+                elif cmd == 'summary': 
+                    # Request condensed summary of conversation
+                    pass
+                elif cmd in ['brief', 'stats']: 
+                    # Get token count and context window size, (and other details, like topic)
+                    pass
+                elif cmd in ['help', '?']: 
+                    # Read thread-level command list from help file 2 and send
+                    pass
+        
     def close(self):
         self.logger.info("Shutting Down")
         
